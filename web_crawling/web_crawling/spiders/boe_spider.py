10220580f1156d925ef0262714a3007f53d3defc
castares <cesar.castares@gmail.com>
2020-12-13 19:25:21 +0100

    [IMP] boe_spider

--- web_crawling/web_crawling/spiders/boe_spider.py
+++ web_crawling/web_crawling/spiders/boe_spider.py
@@ -1,62 +1,114 @@
 import scrapy
 import json
 import os
+from w3lib.html import remove_tags
+
 
 class BoeSpider(scrapy.Spider):
     name = "boe_spider"
-    start_urls = [f"https://boe.es/boe/dias/2020/11/{day}/" for day in ['03','04', '05', '06', '07', '09', '10', '11', '12', '13', '14']]
-    
+    start_urls = [
+        f"https://boe.es/boe/dias/2020/11/{day}/"
+        for day in [
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "09",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+        ]
+    ]
+    # start_urls = ['https://boe.es/diario_boe/ultimo.php']
+
     def parse(self, response):
         for dispo in response.css(".puntoHTML > a::attr('href')").extract():
-            url = response.urljoin(dispo).replace('/txt.php', '/xml.php') 
+            url = response.urljoin(dispo).replace("/txt.php", "/xml.php")
             yield scrapy.Request(url, callback=self.save_xml_dispo)
 
     def save_xml_dispo(self, response):
         tags = {
-            "documento" : response.xpath("//documento/text()").extract_first(),
-            "metadatos" : response.xpath("//metadatos/text()").extract_first(),
-            "identificador" : response.xpath("//identificador/text()").extract_first(),
-            "titulo" : response.xpath("//titulo/text()").extract_first(),
-            "diario" : response.xpath("//diario/text()").extract_first(),
-            "diario_numero" : response.xpath("//diario_numero/text()").extract_first(),
-            "seccion" : response.xpath("//seccion/text()").extract_first(),
-            "subseccion" : response.xpath("//subseccion/text()").extract_first(),
-            "departamento" : response.xpath("//departamento/text()").extract_first(),
-            "rango" : response.xpath("//rango/text()").extract_first(),
-            "numero_oficial" : response.xpath("//numero_oficial/text()").extract_first(),
-            "fecha_disposicion" : response.xpath("//fecha_disposicion/text()").extract_first(),
-            "fecha_publicacion" : response.xpath("//fecha_publicacion/text()").extract_first(),
-            "fecha_vigencia" : response.xpath("//fecha_vigencia/text()").extract_first(),
-            "fecha_derogacion" : response.xpath("//fecha_derogacion/text()").extract_first(),
-            "letra_imagen" : response.xpath("//letra_imagen/text()").extract_first(),
-            "pagina_inicial" : response.xpath("//pagina_inicial/text()").extract_first(),
-            "pagina_final" : response.xpath("//pagina_final/text()").extract_first(),
-            "suplemento_letra_imagen" : response.xpath("//suplemento_letra_imagen/text()").extract_first(),
-            "suplemento_pagina_inicial" : response.xpath("//suplemento_pagina_inicial/text()").extract_first(),
-            "suplemento_pagina_final" : response.xpath("//suplemento_pagina_final/text()").extract_first(),
-            "estatus_legislativo" : response.xpath("//estatus_legislativo/text()").extract_first(),
-            "origen_legislativo" : response.xpath("//origen_legislativo/text()").extract_first(),
-            "estado_consolidacion" : response.xpath("//estado_consolidacion/text()").extract_first(),
-            "judicialmente_anulada" : response.xpath("//judicialmente_anulada/text()").extract_first(),
-            "vigencia_agotada" : response.xpath("//vigencia_agotada/text()").extract_first(),
-            "estatus_derogacion" : response.xpath("//estatus_derogacion/text()").extract_first(),
-            "url_epub" : response.xpath("//url_epub/text()").extract_first(),
-            "url_pdf" : response.xpath("//url_pdf/text()").extract_first(),
-            "url_pdf_catalan" : response.xpath("//url_pdf_catalan/text()").extract_first(),
-            "url_pdf_euskera" : response.xpath("//url_pdf_euskera/text()").extract_first(),
-            "url_pdf_gallego" : response.xpath("//url_pdf_gallego/text()").extract_first(),
-            "url_pdf_valenciano" : response.xpath("//url_pdf_valenciano/text()").extract_first(),
-            "analisis" : response.xpath("//analisis/text()").extract_first(),
-            "notas" : response.xpath("//notas/text()").extract_first(),
-            "materias" : response.xpath("//materias/text()").extract_first(),
-            "alertas" : response.xpath("//alertas/text()").extract_first(),
-            "referencias" : response.xpath("//referencias/text()").extract_first(),
-            "anteriores" : response.xpath("//anteriores/text()").extract_first(),
-            "posteriores" : response.xpath("//posteriores/text()").extract_first(),
-            "texto" : response.xpath("//texto").extract_first(),
+            "documento": response.xpath("//documento/text()").get(),
+            "metadatos": response.xpath("//metadatos/text()").get(),
+            "identificador": response.xpath("//identificador/text()").get(),
+            "titulo": response.xpath("//titulo/text()").get(),
+            "diario": response.xpath("//diario/@codigo").get()
+            or response.xpath("//diario/text()").get(),
+            "diario_numero": response.xpath("//diario_numero/text()").get(),
+            "seccion": response.xpath("//seccion/text()").get(),
+            "subseccion": response.xpath("//subseccion/text()").get(),
+            "departamento": response.xpath("//departamento/text()").get(),
+            "rango": response.xpath("//rango/text()").get(),
+            "numero_oficial": response.xpath("//numero_oficial/text()").get(),
+            "fecha_disposicion": response.xpath("//fecha_disposicion/text()").get(),
+            "fecha_publicacion": response.xpath("//fecha_publicacion/text()").get(),
+            "fecha_vigencia": response.xpath("//fecha_vigencia/text()").get(),
+            "fecha_derogacion": response.xpath("//fecha_derogacion/text()").get(),
+            "letra_imagen": response.xpath("//letra_imagen/text()").get(),
+            "pagina_inicial": response.xpath("//pagina_inicial/text()").get(),
+            "pagina_final": response.xpath("//pagina_final/text()").get(),
+            "suplemento_letra_imagen": response.xpath(
+                "//suplemento_letra_imagen/text()"
+            ).get(),
+            "suplemento_pagina_inicial": response.xpath(
+                "//suplemento_pagina_inicial/text()"
+            ).get(),
+            "suplemento_pagina_final": response.xpath(
+                "//suplemento_pagina_final/text()"
+            ).get(),
+            "estatus_legislativo": response.xpath("//estatus_legislativo/text()").get(),
+            "origen_legislativo": response.xpath("//origen_legislativo/text()").get(),
+            "estado_consolidacion": response.xpath(
+                "//estado_consolidacion/text()"
+            ).get(),
+            "judicialmente_anulada": response.xpath(
+                "//judicialmente_anulada/text()"
+            ).get(),
+            "vigencia_agotada": response.xpath("//vigencia_agotada/text()").get(),
+            "estatus_derogacion": response.xpath("//estatus_derogacion/text()").get(),
+            "url_epub": response.xpath("//url_epub/text()").get(),
+            "url_pdf": response.xpath("//url_pdf/text()").get(),
+            "url_pdf_catalan": response.xpath("//url_pdf_catalan/text()").get(),
+            "url_pdf_euskera": response.xpath("//url_pdf_euskera/text()").get(),
+            "url_pdf_gallego": response.xpath("//url_pdf_gallego/text()").get(),
+            "url_pdf_valenciano": response.xpath("//url_pdf_valenciano/text()").get(),
+            "analisis_notas": response.xpath("//analisis/notas/text()").get(),
+            "analisis_materias": response.xpath("//analisis/materias/text()").get(),
+            "analisis_alertas": response.xpath("//analisis/alertas/text()").get(),
+            "referencias_anteriores": response.xpath(
+                "//referencias/anteriores/text()"
+            ).get(),
+            "referencias_posteriores": response.xpath(
+                "//referencias/posteriores/text()"
+            ).get(),
+            "texto": response.xpath("//texto").getall(),
+            "images": response.xpath("//img/@src").getall(),
         }
+        # Add text with HTML tags removed
+        plain_text = [remove_tags(text, encoding="latin1") for text in tags["texto"]]
+        tags["texto_plano"] = plain_text
+        # Save to JSON file
         path = f"../data/{tags['fecha_publicacion']}/{tags['seccion']}/{tags['subseccion']}/{tags['departamento']}"
-        filename = f"{tags['identificador']}.json" 
+        filename = f"{tags['identificador']}.json"
         os.makedirs(path, exist_ok=True)
         with open(f"{path}/{filename}", "w") as file:
             json.dump(tags, file)
