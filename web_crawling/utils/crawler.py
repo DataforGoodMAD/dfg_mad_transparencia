6b4fa1d375c4cc1f84550c139719789ccbce99b2
César Castañón <cesar.castares@gmail.com>
2021-12-08 10:17:00 +0100

    [ADD] utils/crawler.py to download arbitrary urls or time spans

--- web_crawling/utils/crawler.py
+++ web_crawling/utils/crawler.py
@@ -1,6 +1,19 @@
+from typing import Union
+from datetime import datetime, timedelta
+from click.types import DateTime
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 import click
+
+
+def run_crawler(start_urls) -> None:
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(
+        "boe_spider",
+        domain="boe.es",
+        start_urls=start_urls,
+    )
+    process.start()  # the script will block here until the crawling finishes
 
 
 @click.group()
@@ -9,35 +22,41 @@ def cli():
 
 
 @cli.command()
-@click.argument(
-    "start_urls",
-    nargs=-1,
-)
+@click.argument("start_urls", nargs=-1)
 def urls(start_urls="https://boe.es/diario_boe/ultimo.php"):
-    process = CrawlerProcess(get_project_settings())
-    process.crawl(
-        "boe_spider",
-        domain="boe.es",
-        start_urls=start_urls,
-    )
-    process.start()  # the script will block here until the crawling is finished
+    run_crawler(start_urls)
 
 
-def _parse_interval():
-    pass
-
-
-def _build_urls(datetime_list):
-    pass
+def _build_urls(from_date: datetime, to_date: datetime):
+    urls = []
+    base_url = "https://boe.es/boe/dias"
+    processing_date = from_date
+    while processing_date < to_date:
+        date_str = processing_date.strftime("%Y/%m/%d")
+        urls.append(f"{base_url}/{date_str}")
+        processing_date += timedelta(days=1)
+    print(urls)
+    return urls
 
 
 @cli.command()
-@click.argument(
-    "from_to",
+@click.option(
+    "--from_date",
+    "-f",
+    required=True,
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    help="date to start searching. Format must be YYYY-MM-DD",
 )
-def calendar(from_to):
-    from_str, to_str = from_to
-    print(from_str, to_str)
+@click.option(
+    "--to_date",
+    "-t",
+    required=True,
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    help="date to start searching. Format must be YYYY-MM-DD",
+)
+def calendar(from_date, to_date):
+    start_urls = _build_urls(from_date, to_date)
+    run_crawler(start_urls)
 
 
 if __name__ == "__main__":
